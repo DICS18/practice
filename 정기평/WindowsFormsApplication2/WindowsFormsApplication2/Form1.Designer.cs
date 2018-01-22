a38d627a803389701c2eea219aac9cdcad52d129
DICS18 <35652708+DICS18@users.noreply.github.com>
2018-01-22 11:12:58 +0900

    practice

---
+++ 정기평/WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(699, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(473, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Job";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(473, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Job Time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(473, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Job Cost";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(473, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Due Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(751, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Job Order";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(751, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 15);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Output";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(570, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 25);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "4";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(570, 122);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 25);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "3,1,2,5";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(570, 186);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(126, 25);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "80,100,50,120";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(570, 250);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(126, 25);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "2,3,1,4";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(831, 58);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(136, 25);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.Text = "3,2,1,4";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(831, 122);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(136, 25);
+            this.textBox6.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 414);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ 정기평/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public double Crossover_P = 0.5;
+        public double Mutation_P = 0.05;
+
+        Random rnd = new Random();
+        int[] t; //시간
+        int[] c; //벌금
+        int[] p; //시간누적
+        int[] o; //작업순서
+        int[] t_o; //시간순서 재배열
+        int[] c_o; //벌금 재배열
+        int[] d; //due date
+        int[] d_o; //due date 재배열
+        int n; //작업수
+        int penalty; //총페널티
+        int k;  //시간누적값을 위한 정수
+
+        public struct SOLUTION
+        {
+            public int[] chromo;
+            public int cost;
+        }
+        public SOLUTION[] tt;
+        public SOLUTION[] tt_;
+        public SOLUTION[] Best_chromo;
+        public void initial_solution()//랜덤 배열 10개 생성
+        {
+            tt = new SOLUTION[20];
+            for (int i = 0; i < 20; i++)
+            {
+                int[] n_o = new int[n];
+                for (int j = 0; j < n; j++)
+                {
+                    n_o[j] = rnd.Next(1, n + 1);
+                    int y = 0;
+                    while (y < j)
+                    {
+                        if (n_o[j] == n_o[y])
+                        {
+                            n_o[j] = rnd.Next(1, n + 1);
+                            y = 0;
+                        }
+                        else
+                        {
+                            y = y + 1;
+                        }
+                    }
+                }
+                tt[i].chromo = n_o;
+            }
+
+        }
+        public void Next_gen()
+        {
+            int x = 0;//오름차순정렬
+            while (x < 20)
+            {
+                for (int i = 0; i < tt.Length - 1; i++)
+                {
+                    SOLUTION EMsave = tt[i];
+                    if (tt[i].cost > tt[i + 1].cost)
+                    {
+                        tt[i] = tt[i + 1];
+                        tt[i + 1] = EMsave;
+                    }
+                }
+                x = x + 1;
+            }
+            tt_[0] = tt[0];
+            tt_[1] = tt[1];
+        }
+        public int[] Crossover(int index1, int index2)
+        {
+            int Cutpoint = rnd.Next(0, n);
+            int[] Parent1 = tt[index1].chromo;
+            int[] Parent2 = tt[index2].chromo;
+            int[] Child = new int[n];
+            Child = Parent1;
+
+            if (rnd.NextDouble() < Crossover_P)
+            {
+                for (int i = 0; i <= Cutpoint; i++)
+                {
+                    Child[i] = Parent2[i];
+                }
+            }
+            return Child;
+        }
+        public void Mutation(int[] mmm)
+        {
+            int z;
+            for (int i = 0; i < n; i++)
+            {
+                z = 0;
+                z = mmm[i];
+                if (rnd.NextDouble() < Mutation_P)
+                {
+                    if (i != n - 1)
+                    {
+                        mmm[i] = mmm[n - 1];
+                        mmm[n - 1] = z;
+                    }
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            penalty = 0;
+            k = 0;
+            string Job = textBox1.Text;
+            n = Convert.ToInt32(Job);
+            p = new int[n];
+            string st = textBox2.Text;
+            string[] input1 = st.Split(',');
+            t = new int[input1.Length];
+            string sst = textBox3.Text;
+            string[] input2 = sst.Split(',');
+            c = new int[input2.Length];
+            string ssst = textBox5.Text;
+            string[] input3 = ssst.Split(',');
+            o = new int[input3.Length];
+            t_o = new int[input3.Length];
+            c_o = new int[input3.Length];
+            string sssst = textBox4.Text;
+            string[] input4 = sssst.Split(',');
+            d = new int[input4.Length];
+            d_o = new int[input4.Length];
+            initial_solution();
+            for (int i = 0; i < n; i++)
+            {
+                o[i] = Convert.ToInt32(input3[i]);
+                c[i] = Convert.ToInt32(input2[i]);
+                t[i] = Convert.ToInt32(input1[i]);
+                d[i] = Convert.ToInt32(input4[i]);
+            }
+
+            for (int i = 0; i < tt.Length; i++)//랜덤 시퀀스에 대한 코스트
+            {
+                penalty = 0;
+                k = 0;
+                t_o = new int[n];
+                d_o = new int[n];
+                c_o = new int[n];
+                for (int j = 0; j < n; j++)
+                {
+                    t_o[j] = t[tt[i].chromo[j] - 1];
+                    c_o[j] = c[tt[i].chromo[j] - 1];
+                    d_o[j] = d[tt[i].chromo[j] - 1];
+                    k += t_o[j];
+                    p[j] = k;
+                }
+                for (int j = 0; j < n; j++)
+                {
+                    if (p[j] > d_o[j])
+                    {
+                        penalty += (p[j] - d_o[j]) * c_o[j];
+                    }
+                }
+                tt[i].cost = penalty;
+            }
+            // public void Next_gen()
+            // {
+            //     int x = 0;//오름차순정렬
+            //     while (x<20)
+            //     {
+            //         for (int i = 0; i <tt.Length-1; i++)
+            //         {
+            //             SOLUTION EMsave=tt[i];
+            //             if (tt[i].cost>tt[i+1].cost)
+            //             {
+            //                 tt[i] = tt[i+1];
+            //                 tt[i+1] = EMsave;
+            //             }
+            //         }
+            //         x = x + 1;
+            //     }
+            // }
+            // 
+            //for (int i = 0; i < n; i++)
+            //{
+            //    k += t_o[i];
+            //    p[i] = k;
+            //}
+            //for (int i = 0; i <n; i++)
+            //{
+            //    if (p[i]>d_o[i])
+            //    {
+            //        penalty += (p[i] - d_o[i]) * c_o[i];
+            //    }
+            //    else
+            //    {
+            //        penalty = penalty;
+            //    }
+            //}
+            //for (int i = 0; i < n - 1; i++)
+            //{
+            //    penalty += p[i] * c_o[i + 1];
+            //}
+            textBox6.Text = penalty.ToString();
+
+        }
+    }
+}
